71a85771b246b70f46d5243214ad825449d7b37c
Ryan Tan <ryan.tan@live.com>
2018-05-31 12:12:45 +0800

    Add Kosuke's codes

--- evaluation/convert.py
+++ evaluation/convert.py
@@ -5,9 +5,9 @@ from gensim.models.keyedvectors import KeyedVectors
 
 
 def golve2w2v(src_dir, dst_dir):
-    for file in os.listdir("models\\glove"):
+    for file in os.listdir(src_dir):
         print("Converting {}".format(file))
-        glove2word2vec(src_dir + "\\" + file, dest_dir + "\\" + file)
+        glove2word2vec(src_dir + "\\" + file, dst_dir + "\\" + file)
 
 
 def w2v_txt2bin(src_file, dst_file):
@@ -21,5 +21,8 @@ def w2v_bin2txt(src_file, dst_file):
 
 
 if __name__ == '__main__':
-    # golve2w2v("models\\glove", "models\\txt")
-    w2v_bin2txt('models/txt/fake_model.txt', 'models/bin/fake_model.bin')
+    # golve2w2v("C:\\Users\\two\\Desktop\\May_HackFest\\evaluation\\Jeff", "models\\txt")
+    # w2v_bin2txt('models/txt/fake_model.txt', 'models/bin/fake_model.bin')
+    
+    for file in os.listdir('models/txt'):
+        print(file)
